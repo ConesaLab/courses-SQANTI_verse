6d7ecee21d69a79f651207ff2bf8be0b94fa56c5
pabloati <pabloatienzalo@gmail.com>
2026-02-05 14:58:37 +0100

    SQANTI basic remade

--- scripts/02_QC_module.sh
+++ scripts/02_QC_module.sh
@@ -5,6 +5,6 @@ source activate sqanti3
 
 sqanti3_qc.py \
     --isoforms results/01_isoquant_transcriptome/mouse.transcript_models.clean.gtf \
-    --refGTF data/isoquant/Mus_musculus.GRCm39.104.chr19.gtf \
+    --refGTF data/isoquant/Mus_musculus.GRCm39.115.chr19.gtf \
     --refFasta data/isoquant/Mus_musculus.GRCm39.dna.chr19.fasta \
-    --include_ORF --ir results/02_QC_module --output mouse
+    --include_ORF --dir results/02_QC_module --output mouse
